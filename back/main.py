ed7928ed1c2d58bf5a7ccba0787ebe9261ddc3af
Soléna Toussaint <solena_ananda@hotmail.com>
2024-04-04 16:37:33 +0000

    c'est rangé

--- back/main.py
+++ back/main.py
@@ -1,9 +1,19 @@
+from typing import List
+
 from game import Game
 from selection import Selection
 
-set_game_1 = Game()
-set_game_1.game_play()
+# set_game_1 = Game()
+# set_game_1.game_play()
 
+def my_api(params: List[str]) -> List[str]:
+    # params: input (from front) = full selection
+    # output -> Set
+    params = ["1ORH", "2VVP", "3ORV"]
 
-set_game_2 = Selection()
-set_game_2.find_set()
+    selection_instance = Selection()
+    for param in params:
+        card = Card(param)
+        selection_instance.selection_cards.append(card)
+    selection_instance.find_set()
+
